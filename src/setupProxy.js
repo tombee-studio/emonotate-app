5204cac370a660ae7de44e0cde20b7aca36a1482
Tomoya Bansho <tomomail1997@gmail.com>
2022-05-20 17:44:27 +0900

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,17 +3,17 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 module.exports = app => {
     const staging = process.env.REACT_APP_STAGING;
     if(staging == "local") {
-        app.use("/api/", createProxyMiddleware({
+        app.use("/", createProxyMiddleware({
             target: "http://127.0.0.1:8000/",
             changeOrigin: true
         }));
     } else if(staging == "alpha") {
-        app.use("/api/", createProxyMiddleware({
+        app.use("/", createProxyMiddleware({
             target: "https://enigmatic-thicket-08912.herokuapp.com/",
             changeOrigin: true
         }));
     } else if(staging == "prod") {
-        app.use("/api/", createProxyMiddleware({
+        app.use("/", createProxyMiddleware({
             target: "https://www.emonotate.com/",
             changeOrigin: true
         }));
