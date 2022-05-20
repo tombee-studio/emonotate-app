2e1d708bfae1a70fdd24374581c754139e792ccd
Tomoya Bansho <tomomail1997@gmail.com>
2022-05-20 15:46:37 +0900

    change port

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require('express');
 const setupProxy = require('./src/setupProxy');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Running at Port 3000...');
+app.listen(PORT, () => {
+    console.log(`Running at Port ${PORT}...`);
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
