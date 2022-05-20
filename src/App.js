1335de7965423d9614e09f76777cc50b2c870fcc
Tomoya Bansho <tomomail1997@gmail.com>
2022-05-20 16:56:34 +0900

    環境変数に対応

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ import loadable from "@loadable/component";
 const App = () => {
   const [cookies] = useCookies(undefined);
   if(!cookies.username) {
-    if(process.env.STAGING === "local") {
+    if(process.env.REACT_APP_STAGING === "local") {
       return <Redirect to={"http://127.0.0.1:8000/"} />;
-    } else if(process.env.STAGING === "alpha") {
+    } else if(process.env.REACT_APP_STAGING === "alpha") {
       return <Redirect to={"https://enigmatic-thicket-08912.herokuapp.com/"} />;
-    } else if(process.env.STAGING === "prod") {
+    } else if(process.env.REACT_APP_STAGING === "prod") {
       return <Redirect to={"https://www.emonotate.com/"} />;
     } else {
       console.log(process.env);
