e6f7a56d2befecfccbe4c19cfebae0f5fcf0e162
Tomoya Bansho <tomomail1997@gmail.com>
2022-05-21 13:59:17 +0900

    fix bugs

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 setupProxy(app);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,'./build/index.html'));
+});
 app.listen(PORT, () => {
     console.log(`Running at Port ${PORT}...`);
 });
