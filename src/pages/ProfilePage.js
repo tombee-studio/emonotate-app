3010c720c93f134d38beaff0ca2b2d767d9ca886
Tomoya Bansho <tomomail1997@gmail.com>
2022-06-13 08:15:19 +0900

    Profileを表示

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import Box from '@mui/material/Box';
+import React, { useState } from 'react';
+import { ButtonGroup, Button, Box, Snackbar } from '@mui/material';
 import ProfileComponent from '../components/profile-page/ProfileComponent';
 
+import UserAPI from '../helper/UserAPI';
+
 const ProfilePage = (props) => {
+    const [user, setUserData] = useState(window.django.user);
+    const [snackbarData, setSnackbarData] = useState({
+        is_open: false,
+        message: ""
+    });
+    const handleOnUpdate = ev => {
+        const api = new UserAPI();
+        const _user = { ...user };
+        api.update(_user.id, _user).then(res => {
+            if(res.status == 200) { 
+                setUserData(res.json()); 
+                setSnackbarData({
+                    is_open: true,
+                    message: '更新しました'
+                });
+            }
+            else throw res.detail;
+        });
+    };
+    const generateButton = _user => {
+        return (<Button onClick={handleOnUpdate}>更新</Button>);
+    }
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') return;
+        setSnackbarData({
+            is_open: false,
+            message: ''
+        });
+    };
     return (
         <Box m={2}>
-            <Box m={1}>
-                <ProfileComponent user={window.django.user} />
-            </Box>
+            <ProfileComponent user={user} setUserData={setUserData} />
+            <ButtonGroup>{ generateButton(user) }</ButtonGroup>
+            <Snackbar 
+                open={snackbarData.is_open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+                message={snackbarData.message}/>
         </Box>
     );
 }
