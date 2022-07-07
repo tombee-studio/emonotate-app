8f86dcef3bc94b82be3a3502f899bf036b37ab9b
Tomoya Bansho <tomomail1997@gmail.com>
2022-07-07 18:18:29 +0900

    余白を追加

--- src/components/request-page/RequireListComponent.js
+++ src/components/request-page/RequireListComponent.js
@@ -11,7 +11,8 @@ import {
     Typography, 
     Divider, 
     Pagination,
-    CircularProgress } from '@mui/material';
+    CircularProgress, 
+    Stack} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 import EditIcon from '@mui/icons-material/Edit';
@@ -47,10 +48,7 @@ const RequireListComponent = props => {
         });
     }, []);
 
-    return (loading ? <Box m={2}>
-            <CircularProgress />
-        </Box> :
-        <Box m={2}>
+    return (<Box m={2}>
             <Typography
                 component="div"
                 variant="h6"
@@ -65,66 +63,70 @@ const RequireListComponent = props => {
                     <AddIcon />
                 </IconButton>
             </Typography>
-            <Pagination 
-                count={result.pagination.total_pages}
-                variant="outlined" 
-                shape="rounded"
-                onChange={handlePaginate} />
             <Divider />
-            <List>
-                {
-                    result.models.map(request => {
-                        return (
-                            <ListItem
-                                button
-                                component="a"
-                                href={`/app/rooms/${request.id}`}
-                                key={request.room_name}>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <MeetingRoomIcon />
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={request.title}
-                                    secondary={
-                                        request.description.length > 30?
-                                        request.description.substr(0, 30) + "...":
-                                        request.description
-                                    }
-                                />
-                                <ListItemSecondaryAction>
-                                    <IconButton
+            {loading ? 
+                <Box m={2}><CircularProgress /></Box> : 
+                <Stack m={2}>
+                    <Pagination 
+                        count={result.pagination.total_pages}
+                        variant="outlined" 
+                        shape="rounded"
+                        onChange={handlePaginate} />
+                    <List>
+                        {
+                            result.models.map(request => {
+                                return (
+                                    <ListItem
+                                        button
                                         component="a"
-                                        href={`/app/requests/${request.id}`}
-                                        edge="end"
-                                        aria-label="enter"
-                                        size="large">
-                                        <EditIcon />
-                                    </IconButton>
-                                    <IconButton
-                                        component="a"
-                                        edge="end"
-                                        aria-label="delete"
-                                        onClick={_ => {
-                                            this.api.delete(request.id, {
-                                                'format': 'json'
-                                            })
-                                            .then(res => {
-                                                if(res.status == 200 || res.status == 204) {
-                                                    window.location.href = '/app/requests/';
-                                                }
-                                            });
-                                        }}
-                                        size="large">
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </ListItemSecondaryAction>
-                            </ListItem>
-                        );
-                    })
-                }
-            </List>
+                                        href={`/app/rooms/${request.id}`}
+                                        key={request.room_name}>
+                                        <ListItemAvatar>
+                                            <Avatar>
+                                                <MeetingRoomIcon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary={request.title}
+                                            secondary={
+                                                request.description.length > 30?
+                                                request.description.substr(0, 30) + "...":
+                                                request.description
+                                            }
+                                        />
+                                        <ListItemSecondaryAction>
+                                            <IconButton
+                                                component="a"
+                                                href={`/app/requests/${request.id}`}
+                                                edge="end"
+                                                aria-label="enter"
+                                                size="large">
+                                                <EditIcon />
+                                            </IconButton>
+                                            <IconButton
+                                                component="a"
+                                                edge="end"
+                                                aria-label="delete"
+                                                onClick={_ => {
+                                                    this.api.delete(request.id, {
+                                                        'format': 'json'
+                                                    })
+                                                    .then(res => {
+                                                        if(res.status == 200 || res.status == 204) {
+                                                            window.location.href = '/app/requests/';
+                                                        }
+                                                    });
+                                                }}
+                                                size="large">
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
+                                    </ListItem>
+                                );
+                            })
+                        }
+                    </List>
+                </Stack>}
         </Box>
     );
 };
--- src/pages/RequestPage.js
+++ src/pages/RequestPage.js
@@ -1,33 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Card} from '@mui/material';
+import React from 'react';
+import { Box, Card } from '@mui/material';
 import RequestListComponent from '../components/request-page/RequestListComponent';
 import RequireListComponent from '../components/request-page/RequireListComponent';
-import RequestListAPI from '../helper/RequestListAPI';
 
 export default function RequestPage(props) {
-  const [request, setRequest] = useState([]);
-  useEffect(() => {
-    const requestAPI = new RequestListAPI();
-    Promise.all([
-      new Promise(resolve => {
-        return requestAPI.get({
-          'format': 'json',
-          'role': 'participant',
-        })
-        .then(json => {
-          resolve(json.models);
-        });
-      })
-    ])
-    .then(values => {
-      const [_requests, _requires] = values;
-      setRequest(_requests);
-    });
-  }, []);
   return (
     <Box m={2}>
         <Card m={2}>
-            <RequestListComponent requests={request} />
+            <RequestListComponent />
             <RequireListComponent />
         </Card>
     </Box>
