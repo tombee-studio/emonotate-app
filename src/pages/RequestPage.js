1b4448c070be7d3ab4b018b0c36f00d09f3de32e
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-08-22 10:36:32 +0900

    研究者アカウントでのみリクエスト作成機能が使用できる

--- src/pages/RequestPage.js
+++ src/pages/RequestPage.js
@@ -3,13 +3,25 @@ import { Box, Card } from '@mui/material';
 import RequestListComponent from '../components/request-page/RequestListComponent';
 import RequireListComponent from '../components/request-page/RequireListComponent';
 
-export default function RequestPage(props) {
+const RequestPage = (props) => {
+  const { user } = window.django;
+
+  const createReuquestList = user => {
+    const list = [];
+    list.push(<RequestListComponent />);
+    if(user.groups.includes("Researchers")) {
+      list.push(<RequireListComponent />);
+    }
+    return list;
+  };
+
   return (
     <Box m={2}>
         <Card m={2}>
-            <RequestListComponent />
-            <RequireListComponent />
+          { createReuquestList(user) }
         </Card>
     </Box>
   );
 }
+
+export default RequestPage;
