555c9ad7b13e81bdcfce2d5423ebafe4f513f286
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-10-05 23:54:54 +0900

    リクエストの表示部分をMarkdownで表示

--- src/components/curve-page/RequestDetailComponent.js
+++ src/components/curve-page/RequestDetailComponent.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import { Typography } from "@mui/material";
 import { Box } from "@mui/material";
+import ReactMarkdown from "react-markdown";
 
 const RequestDetailComponent = props => {
     const { request } = props;
     return <Box m={2}>
         <Typography color="textPrimary">{request.title}</Typography>
-        <Typography color="textSecondary">{request.description}</Typography>
+        <ReactMarkdown children={request.description} />
     </Box>;
 };
 
