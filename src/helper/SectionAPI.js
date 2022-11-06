ab6f6e9404749be9a86c15535715d118416333b5
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-11-06 23:21:10 +0900

    Section APIをcallするクラスを作成

---
+++ src/helper/SectionAPI.js
@@ -0,0 +1,52 @@
+export default class SectionAPI {
+    create(data, queries={
+      'format': 'json'
+    }) {
+      const query = Object.keys(queries).map(key => `${key}=${queries[key]}`).join('&');
+      return fetch(`/api/sections/?${query}`, {
+          method: 'post',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': window.django.csrf,
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(res => {
+          if(res.status != 200 && res.status != 201) throw res;
+          return res.json();
+        });
+    }
+  
+    update(data, queries={
+      'format': 'json'
+    }) {
+      const query = Object.keys(queries).map(key => `${key}=${queries[key]}`).join('&');
+      return fetch(`/api/sections/${data.id}/?${query}`, {
+          method: 'put',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': window.django.csrf,
+          },
+          body: JSON.stringify(data)
+        })
+        .then(res => {
+          if(res.status != 200 && res.status != 201) throw res;
+          return res.json();
+        });
+    }
+  
+    delete(item) {
+      return fetch(`/api/sections/${item}`, {
+        method: 'delete',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': window.django.csrf,
+        }
+      })
+    }
+  };
+  
