24bf8d30e1f323dfc91f23280857b22efd384e35
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-11-14 15:24:21 +0900

    修正

--- public/static/users/js/youtubeView.js
+++ public/static/users/js/youtubeView.js
@@ -8,3 +8,30 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 var done;
 
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: youtubeId,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+} 
+
