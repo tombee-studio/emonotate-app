de817b1a93b6a0df0bcc4e21cee4ec44e43e309d
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-11-16 14:24:50 +0900

    各作業をクラスに包含

--- public/static/users/js/youtubeView.js
+++ public/static/users/js/youtubeView.js
@@ -1,37 +1,28 @@
-/*youtube*/
+function YouTubeView(youtubePlayerID, videoId) {
+    this.onYouTubeIframeAPIReady = function() {
+        this.player = new YT.Player(youtubePlayerID, {
+            height: '360',
+            width: '640',
+            videoId: videoId,
+            events: {
+                'onReady': function(event) {
+                    event.target.playVideo();
+                }.bind(this),
+                'onStateChange': function(event) {
+                    if (event.data == YT.PlayerState.PLAYING && !done) {
+                        setTimeout(stopVideo, 6000);
+                        done = true;
+                    }
+                }.bind(this)
+            }
+        });
+    }.bind(this);
 
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    this.stopVideo = function() {
+        this.player.stopVideo();
+    }.bind(this);
 
-var player;
-var done;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '360',
-        width: '640',
-        videoId: youtubeId,
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
+    this.initialize = function() {
+        YT.ready(this.onYouTubeIframeAPIReady);
+    }.bind(this);
 }
-
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
-} 
-
