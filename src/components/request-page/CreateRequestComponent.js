012d749e160e7f61e18207362905d6e50c2f88eb
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-10 19:04:03 +0900

    リクエスト機能の追加

---
+++ src/components/request-page/CreateRequestComponent.js
@@ -0,0 +1,40 @@
+import { ButtonGroup, Button } from "@mui/material";
+import React, { useState } from "react";
+import RequestListAPI from "../../helper/RequestListAPI";
+import ObserverComponent from "./ObserverComponent";
+
+const CreateRequestComponent = props => {
+    const { id } = props;
+    const [isLoadedRequest, setFlagLoadedRequest] = useState(false);
+    const [request, setRequest] = useState({});
+    const create = ev => {
+        const api = new RequestListAPI();
+        const req = {}
+        req.title = request.title;
+        req.description = request.description;
+        req.content = request.content.id;
+        req.value_type = request.value_type.id;
+        req.owner = window.django.user.id;
+        req.values = request.values;
+        req.is_required_free_hand = request.is_required_free_hand;
+
+        api.create(req, { "format": "json" })
+        .then(data => {
+            window.location.href = `/app/requests/${data.id}`;
+        });
+    };
+    const createContent = () => {
+        return <>
+            <ObserverComponent 
+                request={request}
+                onChange={ req => setRequest(req)}
+            />
+            <ButtonGroup>
+                <Button onClick={create}>作成</Button>
+            </ButtonGroup>
+        </>
+    };
+    return createContent();
+};
+
+export default CreateRequestComponent;
