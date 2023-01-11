d221a8710a455c427aa830a547bae259164bd773
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-11 10:44:22 +0900

    remove comment

--- public/static/users/js/ButtonAction.js
+++ public/static/users/js/ButtonAction.js
@@ -3,7 +3,6 @@ function getCookie(name) {
     if (document.cookie && document.cookie !== '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
-            console.log(cookies[i]);
             var cookie = cookies[i];
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
