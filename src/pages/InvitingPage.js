958854662a640d4aca65a7b83dd9a96dfd2750b1
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-12 13:47:42 +0900

    InvitingPageを修正

--- src/pages/InvitingPage.js
+++ src/pages/InvitingPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Stack } from "@mui/material";
+
+import GenerateInvitingTokenComponent from "../components/inviting-page/GenerateInvitingTokenComponent";
+
 const InvitingPage = props => {
-    return <></>;
+    return <Stack m={2}>
+        <GenerateInvitingTokenComponent />
+    </Stack>;
 };
 
 export default InvitingPage;
