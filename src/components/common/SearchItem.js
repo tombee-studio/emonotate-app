bd1bccae0af0521ec2fa0830a19058be77119553
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-31 11:53:53 +0900

    add button group

--- src/components/common/SearchItem.js
+++ src/components/common/SearchItem.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import { 
+    ButtonGroup,
+    IconButton,
     ImageListItem, ImageListItemBar
 } from '@mui/material';
+import { LineAxis, Moving } from '@mui/icons-material';
 
 const SearchItem = props => {
     const { item } = props;
     const { snippet } = item;
     return (
         <ImageListItem 
-            key={item.id.videoId} 
-            component="a"
-            href={`/app/curves/?videoId=${item.id.videoId}&title=${item.snippet.title}`}>
+            key={item.id.videoId} >
             <img
                 srcSet={snippet.thumbnails["medium"].url}
                 alt={item.title}
@@ -21,6 +22,20 @@ const SearchItem = props => {
                 position="below"
                 title={snippet.title}
                 subtitle={`by ${snippet.channelTitle}`}
+                actionIcon={
+                    <ButtonGroup>
+                        <IconButton 
+                            component="a"
+                            href={`/fold-line/?video_id=${item.id.videoId}`}>
+                            <LineAxis />
+                        </IconButton>
+                        <IconButton 
+                            component="a" 
+                            href={`/free-hand/?video_id=${item.id.videoId}`}>
+                            <Moving />
+                        </IconButton>
+                    </ButtonGroup>
+                }
             />
         </ImageListItem>);
 };
