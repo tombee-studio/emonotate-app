c5c5b32a90c50cbc52cffe2a981bb47dd7bc8fb1
tomoya-kwansei <32319810+tomoya-kwansei@users.noreply.github.com>
2023-03-08 17:40:21 +0900

    Revert "use envrc to change enviroment variables"

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,21 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 const cors = require("cors");
 
 module.exports = app => {
-    let targetUrl = process.env.REACT_APP_API_URL;
+    const staging = process.env.REACT_APP_STAGING || "prod";
+    var targetUrl = "";
+    if(staging == "local") {
+        targetUrl = "http://127.0.0.1:8000/";
+    } else if(staging == "alpha") {
+        targetUrl = "https://enigmatic-thicket-08912.herokuapp.com/";
+    } else if(staging == "prod") {
+        if("K_SERVICE" in process.env) {
+            targetUrl = "https://emonotate-service-backend-b7ramgi3ga-an.a.run.app/";
+        } else {
+            targetUrl = "https://www.emonotate.com/";
+        }
+    } else {
+        throw `環境変数 STAGING: ${process.env.STAGING}に設定されています`;
+    }
     app.use("/api/**", createProxyMiddleware({
         target: targetUrl,
         changeOrigin: true
